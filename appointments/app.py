b239752bc2b5f9d1cd3122218b5e6d1f7df07d46
Ahmed Baig <ahmedbaig137@gmail.com>
2023-12-13 04:11:55 +0500

    Added mongoDB to appointments and doctors

--- appointments/app.py
+++ appointments/app.py
@@ -1,27 +1,40 @@
 from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from bson import json_util
+
 app = Flask(__name__)
 
-appointments = [
-  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
-  { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-  { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-]
+# MongoDB connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client.myDatabase  # Replace 'myDatabase' with your desired database name
+appointments = db.appointments
 
 @app.route('/hello')
 def hello():
-  greeting = "Hello world!"
-  return greeting
+    greeting = "Hello world!"
+    return greeting
 
 @app.route('/appointments', methods=["GET"])
 def getAppointments():
-  return jsonify(appointments)
+    all_appointments = appointments.find()
+    return json_util.dumps(all_appointments)
 
 @app.route('/appointment/<id>', methods=["GET"])
 def getAppointment(id):
-  id = int(id) - 1
-  return jsonify(appointments[id])
+    appointment = appointments.find_one({'id': id})
+    return json_util.dumps(appointment) if appointment else ('', 404)
+
+@app.route('/bootstrap', methods=['GET'])
+def bootstrap():
+    initial_appointments = [
+        {'id': "1", 'doctor': "1", 'date': "21 Nov 2023", 'rating': "Good"},
+        {'id': "2", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
+        { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+        { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+        { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+    ]
+    appointments.insert_many(initial_appointments)
+    return jsonify({"message": "Database initialized"}), 200
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=7070)
+    app.run(host="0.0.0.0", port=3000)
