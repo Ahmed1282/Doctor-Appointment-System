a688e8c6c5eb5d113534f4a604908d5adbaa1b13
Ahmed Baig <ahmedbaig137@gmail.com>
2023-12-13 22:51:18 +0500

    Updated files without DB

--- appointments/app.py
+++ appointments/app.py
@@ -1,40 +1,27 @@
 from flask import Flask, jsonify, request
-from pymongo import MongoClient
-from bson import json_util
-
 app = Flask(__name__)
 
-# MongoDB connection
-client = MongoClient("mongodb://localhost:27017/")
-db = client.myDatabase  # Replace 'myDatabase' with your desired database name
-appointments = db.appointments
+appointments = [
+  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
+  { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+  { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+]
 
 @app.route('/hello')
 def hello():
-    greeting = "Hello world!"
-    return greeting
+  greeting = "Hello world!"
+  return greeting
 
 @app.route('/appointments', methods=["GET"])
 def getAppointments():
-    all_appointments = appointments.find()
-    return json_util.dumps(all_appointments)
+  return jsonify(appointments)
 
 @app.route('/appointment/<id>', methods=["GET"])
 def getAppointment(id):
-    appointment = appointments.find_one({'id': id})
-    return json_util.dumps(appointment) if appointment else ('', 404)
-
-@app.route('/bootstrap', methods=['GET'])
-def bootstrap():
-    initial_appointments = [
-        {'id': "1", 'doctor': "1", 'date': "21 Nov 2023", 'rating': "Good"},
-        {'id': "2", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
-        { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-        { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-        { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-    ]
-    appointments.insert_many(initial_appointments)
-    return jsonify({"message": "Database initialized"}), 200
+  id = int(id) - 1
+  return jsonify(appointments[id])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+  app.run(host="0.0.0.0",port=7071)
--- doctors/app.py
+++ doctors/app.py
@@ -1,38 +1,24 @@
 from flask import Flask, jsonify, request
-from pymongo import MongoClient
-from bson import json_util
-
 app = Flask(__name__)
 
-# MongoDB connection
-client = MongoClient("mongodb://localhost:27017/")
-db = client.myDatabase  # Replace 'myDatabase' with your desired database name
-doctors = db.doctors
+doctors = [
+  { 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
+  { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
+]
 
 @app.route('/hello')
 def hello():
-    greeting = "Hello world!"
-    return greeting
+  greeting = "Hello world!"
+  return greeting
 
 @app.route('/doctors', methods=["GET"])
 def getDoctors():
-    all_doctors = doctors.find()
-    return json_util.dumps(all_doctors)
+  return jsonify(doctors)
 
 @app.route('/doctor/<id>', methods=["GET"])
 def getDoctor(id):
-    doctor = doctors.find_one({'id': id})
-    return json_util.dumps(doctor) if doctor else ('', 404)
-
-@app.route('/bootstrap', methods=['GET'])
-def bootstrap():
-    initial_doctors = [
-        {'id': "1", 'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality': "DevOps"},
-        {'id': "2", 'firstName': "Good", 'lastName': "Doctor", 'speciality': "Test"}
-        # Add more initial data if needed
-    ]
-    doctors.insert_many(initial_doctors)
-    return jsonify({"message": "Database initialized"}), 200
+  id = int(id) - 1
+  return jsonify(doctors[id])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9090)
+  app.run(host="0.0.0.0",port=9090)
